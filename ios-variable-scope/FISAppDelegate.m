9b0078fd4d7f19c358ea86bb2e38c6f44a781a22
JohnPH75 <johnhussain75@gmail.com>
2016-05-11 10:03:05 -0400

    thirteenth commit

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -25,4 +25,40 @@
  
  */
 
+
+- (NSMutableArray*)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array {
+    
+    NSMutableArray *arrayCopy = [array mutableCopy];  //copy created
+                               
+    [arrayCopy addObject:string];  //array with string added
+    
+    return arrayCopy;  //returning array
+    
+}
+//returning an array with string added to it.  Array was not modified, a copy was created.
+
+
+- (NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array {
+    
+    NSMutableArray *mArray = [array mutableCopy];   //Array has to be defined first.
+    
+    NSUInteger allCapCount = 0;    // allCapCount used for counting all uppercase objects.
+
+    for (NSUInteger i = 0; i < mArray.count; i++) {   //for loop iterates over array.
+        
+        NSString *capitalizedString = [mArray[i] uppercaseString]; //string is capitalizing objects in array.
+        
+        if ([capitalizedString isEqualToString: mArray[i]]) {
+            
+            allCapCount ++;
+        }
+    }
+    return allCapCount;
+}
+
+- (void) removeAllObjectsFromArray:(NSMutableArray *)array {
+    [array removeAllObjects]; //empties array of objects
+}
+
+
 @end
